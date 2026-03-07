8a4fc1da5627cb659ddbd4a40571b5aaf87d491d
ocatania <ocatania@uoguelph.ca>
2026-03-06 19:18:38 -0500

    Fix: Explicit typing and xlsx typing

--- backend/src/main/java/com/example/cis4900/spring/template/ingest/controller/IngestDataController.java
+++ backend/src/main/java/com/example/cis4900/spring/template/ingest/controller/IngestDataController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.MediaType;
 import com.example.cis4900.spring.template.ingest.service.IngestDataService;
 
 @RestController
