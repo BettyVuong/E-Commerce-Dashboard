e0ea48d8bcfe23e269ff1778f12ed7eee8532ec2
Julia Horner <jhorne03@uoguelph.com>
2026-03-11 18:17:22 -0400

    Chore: fix checkstyle violation in controller and model

--- backend/src/main/java/com/example/cis4900/spring/template/rfm/controller/RfmController.java
+++ backend/src/main/java/com/example/cis4900/spring/template/rfm/controller/RfmController.java
@@ -3,7 +3,11 @@ package com.example.cis4900.spring.template.rfm.controller;
 import com.example.cis4900.spring.template.rfm.model.RfmMetric;
 import com.example.cis4900.spring.template.rfm.service.RfmService;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
--- backend/src/main/java/com/example/cis4900/spring/template/rfm/model/RfmMetric.java
+++ backend/src/main/java/com/example/cis4900/spring/template/rfm/model/RfmMetric.java
@@ -3,13 +3,18 @@ package com.example.cis4900.spring.template.rfm.model;
 import java.math.BigDecimal;
 
 /**
- * interface based projection to hold results of RFM aggregation query
+ * interface based projection to hold results of RFM aggregation query.
  */
 public interface RfmMetric {
     String getCustomerID();
+
     Integer getRecency(); // x-axis: number of days
-    Long getOrderID(); // y-axis frequency           
+
+    Long getOrderID(); // y-axis frequency    
+
     BigDecimal getTotalAmount();  // raw monetary value for hover
+
     String getCountry();
+    
     Double getBubbleSize(); // normalize scale
 }
