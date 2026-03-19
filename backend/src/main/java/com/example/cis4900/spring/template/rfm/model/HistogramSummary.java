e026daae54b9a913d0b54e5e9fd26154ed65dc28
ocatania <ocatania@uoguelph.ca>
2026-03-19 19:59:36 -0400

    Fix: Checkstyle formatting

--- backend/src/main/java/com/example/cis4900/spring/template/rfm/model/HistogramSummary.java
+++ backend/src/main/java/com/example/cis4900/spring/template/rfm/model/HistogramSummary.java
@@ -1,6 +1,6 @@
 package com.example.cis4900.spring.template.rfm.model;
 
-public record HistogramSummary (
+public record HistogramSummary(
     int invoiceCount,
     Double average,
     Double median,
--- backend/src/main/java/com/example/cis4900/spring/template/rfm/repository/InvoiceHistogramProjection.java
+++ backend/src/main/java/com/example/cis4900/spring/template/rfm/repository/InvoiceHistogramProjection.java
@@ -2,6 +2,8 @@ package com.example.cis4900.spring.template.rfm.repository;
 
 public interface InvoiceHistogramProjection {
     String getInvoice();
+
     Number getBasketSize();
+
     Number getOrderValue();
 }
